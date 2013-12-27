61dd2cc744b0c4d84b51511f7bbd31f603ea9dd8
Christopher Kollars <chris@christopherkollars.com>
2013-12-26 21:30:23 -0600

    init repo

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# source 'https://rubygems.org'
+
+group :development do
+
+  # Sass, Compass and extensions.
+  gem 'sass'                    # Sass.
+  gem 'sass-globbing'           # Import Sass files based on globbing pattern.
+  gem 'compass'                 # Framework built on Sass.
+  gem 'compass-rgbapng'         # Turns rgba() into .png's for backwards compatibility.
+  gem 'toolkit'                 # Compass utility from the fabulous Snugug.
+
+end
---
+++ Gruntfile.js
@@ -0,0 +1,122 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    siteConfig: grunt.file.readJSON('site-config.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+      ' <%= pkg.license %> License */\n',
+    grunticon: {
+      icons: {
+        options: {
+          src: "svg/",
+          dest: "scss/partials/utility/",
+          datasvgcss: "_icons.data.svg.scss",
+          datapngcss: "_icons.data.png.scss",
+          urlpngcss: "_icons.fallback.scss"
+        }
+      }
+    },
+     // Includes allow us to have partials in HTML
+    includes: {
+      files: {
+        src: ['*.html','*.php'],
+        dest: '_dev/',
+        flatten: true,
+        debug: true
+      }
+    },
+    compass: {
+      options: {
+        // bundleExec: true,
+        cssDir: '_dev/css',
+        sassDir: 'scss',
+        imagesDir: 'images',
+        javascriptsDir: 'js',
+        fontsDir: 'fonts',
+        assetCacheBuster: 'none',
+        require: [
+          'sass-globbing',
+          'toolkit',
+          'rgbapng'
+        ]
+      },
+      dev: {
+        options: {
+          environment: 'development',
+          outputStyle: 'expanded',
+          relativeAssets: true,
+          raw: 'line_numbers = :true\n'
+        }
+      },
+      dist: {
+        options: {
+          environment: 'production',
+          outputStyle: 'compact',
+          force: true
+        }
+      }
+    },
+    watch: {
+      html: {
+        files: ['*.html', '*.php'],
+        tasks: ['includes']
+      },
+      compass: {
+        files: ['scss/{,**/,**/**}*.scss'],
+        tasks: ['compass:dev']
+      },
+      js: {
+        files: ['js/*.js','js/vendor/*.js'],
+        tasks: ['concat','uglify']
+      },
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: [
+          'css/style.css',
+          'js/*.js',
+          '*.html',
+          'images/{,**/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+    copy: {
+      dev: {
+        files: [
+          {expand: true, cwd: '', src: ['scss/partials/utility/png/**'], dest: '_dev/images/'},
+          {expand: true, cwd: '', src: ['images/**/*'], dest: '_dev/'},
+          {expand: true, cwd: '', src: ['js/no-concat/modernizr.js'], dest: '_dev/'},
+          {expand: true, cwd: '', src: ['partials/chamber.php'], dest: '_dev/'},
+        ]
+      }
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/vendor/*.js','js/*.js'],
+        dest: '_dev/js/compiled.js'
+      }
+    },
+    uglify: {
+      build: {
+        files: {
+          '_dev/js/compiled.min.js' : '_dev/js/compiled.js'
+        }
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('images', ['grunticon']);
+  grunt.registerTask('assets', ['copy:dev','includes','compass:dev','concat','uglify']);
+};
